5c2f79e642d2bf0d26b3b46d4936d30fb12018d8
arpit-batra <arpitbatra98@gmail.com>
2018-07-10 12:42:25 +0530

    UserName and Password displayed in the Logs

--- app/src/main/java/com/example/android/demoapp/MainActivity.java
+++ app/src/main/java/com/example/android/demoapp/MainActivity.java
@@ -1,12 +1,23 @@
 package com.example.android.demoapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    public void clickFunction(View view)
+    {
+        EditText userName = findViewById(R.id.userName);
+        EditText pas = findViewById(R.id.pas);
+        Log.i("info","Your username is "+userName.getText().toString()+" and your password is "+pas.getText().toString() );
+    }
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
